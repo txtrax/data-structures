c3993475dfab952605dc43fa69df3237dfa5b12c
Camden Smith <camdensmithh@gmail.com>
2022-06-03 16:10:13 -0700

    Commit linkedList & tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,8 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,9 +11,24 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = Tree(value);
+  //Push value to children array
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  //base case
+  if (this.value === target) {
+    return true;
+  }
+  //recursion case
+  let child = this.children;
+  for (let i = 0; i < child.length; i++) {
+    if (child[i].contains(target)) {
+      return true;
+    };
+  }
+  return false;
 };
 
 
