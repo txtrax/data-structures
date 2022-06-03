796a33e04de13792a314941cd16fb8c0cae24e2c
Camden Smith <camdensmithh@gmail.com>
2022-06-02 19:10:09 -0700

    Data structures part 1 complete.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,27 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    let keys = Object.keys(storage);
+    let last = keys[keys.length - 1];
+
+    storage[last + 1] = value;
   };
 
   someInstance.dequeue = function() {
+    let keys = Object.keys(storage);
+    let first = keys[0];
+    let dequeued = storage[first];
+
+    delete storage[first];
+
+    return dequeued;
   };
 
   someInstance.size = function() {
+    let keys = Object.keys(storage);
+
+    return keys.length;
   };
 
   return someInstance;
--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Queue.prototype.enqueue = function(value) {
+  let keys = Object.keys(this.storage);
+  let last = keys[keys.length - 1];
 
+  this.storage[last + 1] = value;
+};
+
+Queue.prototype.dequeue = function() {
+  let keys = Object.keys(this.storage);
+  let first = keys[0];
+  let dequeued = this.storage[first];
+
+  delete this.storage[first];
+
+  return dequeued;
+};
+
+Queue.prototype.size = function() {
+  let keys = Object.keys(this.storage);
+
+  return keys.length;
+};
+
+const queue1 = new Queue();
