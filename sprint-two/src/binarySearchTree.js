ebf03c1b712b9b5bd16d043e31d69732e9462b60
Camden Smith <camdensmithh@gmail.com>
2022-06-04 12:32:27 -0700

    Completed binarySearchTree and set

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -8,26 +8,30 @@ BinarySearchTree.prototype.insert = function(val) {
   //if value less than right add to left
   if (val < this.value) {
     if (this.left === null) {
-      this.left = BinarySearchTree(val);
+      this.left = new BinarySearchTree(val);
     } else {
       this.left.insert(val);
     }
   } else if (val > this.value) {
     if (this.right === null) {
-      this.right = BinarySearchTree(val);
+      this.right = new BinarySearchTree(val);
     } else {
       this.right.insert(val);
     }
+  } else {
+    this.value = val;
   }
-  this.value = val;
 };
 
 BinarySearchTree.prototype.contains = function(val) {
   if (val === this.value) {
     return true;
-  } else if (val < this.value) {
+  }
+
+  if (val < this.value && this.left !== null) {
     return this.left.contains(val);
-  } else if (val > this.value) {
+  }
+  if (val > this.value && this.right !== null) {
     return this.right.contains(val);
   }
 
@@ -35,11 +39,20 @@ BinarySearchTree.prototype.contains = function(val) {
 };
 
 BinarySearchTree.prototype.depthFirstLog = function(func) {
+  //recursive base case
+  func(this.value);
 
+  //recursive case
+  if (this.left) {
+    this.left.depthFirstLog(func);
+  }
+  if (this.right) {
+    this.right.depthFirstLog(func);
+  }
 };
 
-// var tree = new BinarySearchTree(4);
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * all operations of binarySearchTree are linear
  */
