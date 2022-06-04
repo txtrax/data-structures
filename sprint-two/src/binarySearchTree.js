9e64832229728e0ba10772b4fd003276c90e25da
Camden Smith <camdensmithh@gmail.com>
2022-06-03 17:16:14 -0700

    binarySearchTree 2/3 complete

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,44 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
 
+BinarySearchTree.prototype.insert = function(val) {
+  //if value less than right add to left
+  if (val < this.value) {
+    if (this.left === null) {
+      this.left = BinarySearchTree(val);
+    } else {
+      this.left.insert(val);
+    }
+  } else if (val > this.value) {
+    if (this.right === null) {
+      this.right = BinarySearchTree(val);
+    } else {
+      this.right.insert(val);
+    }
+  }
+  this.value = val;
+};
+
+BinarySearchTree.prototype.contains = function(val) {
+  if (val === this.value) {
+    return true;
+  } else if (val < this.value) {
+    return this.left.contains(val);
+  } else if (val > this.value) {
+    return this.right.contains(val);
+  }
+
+  return false;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(func) {
+
+};
+
+// var tree = new BinarySearchTree(4);
 
 /*
  * Complexity: What is the time complexity of the above functions?
